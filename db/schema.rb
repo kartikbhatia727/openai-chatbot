4f0b8a8e9dce372f6d46d2e62567814477442091
kartik <90633037+kbhatia727@users.noreply.github.com>
2025-03-09 00:57:51 -0500

    Working model
    OpenAi as an activeJob --> next step

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_08_041806) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_09_001616) do
+  create_table "chats", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
+    t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "chat_id", null: false
+    t.text "content"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_08_041806) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "chats", "users"
+  add_foreign_key "messages", "chats"
 end
